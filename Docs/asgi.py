8c76076c5ced5958260d2768e6ec6aeb0726e554
Dav <david@gmail.com>
2021-05-21 15:29:57 -0500

    first commit, installing django and rest

---
+++ Docs/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Docs project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Docs.settings')
+
+application = get_asgi_application()
